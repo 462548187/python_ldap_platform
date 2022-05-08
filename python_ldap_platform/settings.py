940ce000eb9a2b27b69d155694704b08d9db2aa6
liuyue <462548187@qq.com>
2022-05-08 17:47:48 +0800

    fix config

--- python_ldap_platform/settings.py
+++ python_ldap_platform/settings.py
@@ -16,7 +16,7 @@ import configparser
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 config = configparser.ConfigParser()
-config.read(os.path.join(BASE_DIR, 'config.yml'))
+config.read(os.path.join(BASE_DIR, 'conf.yml'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
